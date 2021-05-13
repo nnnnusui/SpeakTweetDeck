2ba93500500cafa302d9caf43d2bb286f2b10df0
nnnnusui <nnnnusui@gmail.com>
2021-05-14 03:21:20 +0900

    update: add speak button

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-const speak = (tweetNode: Node) => {
-  const tweetElement = tweetNode as Element;
+const addSpeakButton = (tweetElement: Element) => {
+  const actionList = tweetElement.getElementsByClassName("tweet-actions")[0];
+  if (!actionList) return;
+  const button = document.createElement("a");
+  button.href = "#";
+  button.rel = "speak";
+  button.classList.add("tweet-action");
+  button.textContent = "🔈";
+  button.onclick = () => speak(tweetElement);
+  const container = document.createElement("li");
+  container.appendChild(button);
+  actionList.appendChild(container);
+};
+
+const isSpeakTarget = (tweetElement: Element) => true;
+const speak = (tweetElement: Element) => {
   const text =
     tweetElement.getElementsByClassName("js-tweet-text")[0].textContent;
   const utterance = new SpeechSynthesisUtterance(text ? text : "");
@@ -8,19 +22,28 @@ const speak = (tweetNode: Node) => {
   console.log(utterance);
 };
 
+const onDetectTweet = (tweetNode: Node) => {
+  const tweetElement = tweetNode as Element;
+  addSpeakButton(tweetElement);
+  if (isSpeakTarget(tweetElement)) speak(tweetElement);
+};
+
 const observer = new MutationObserver((mutations) =>
   mutations.forEach((mutation) =>
-    Array.from(mutation.addedNodes).reverse().forEach(speak)
+    Array.from(mutation.addedNodes)
+      .reverse()
+      .forEach((it) => onDetectTweet(it as Element))
   )
 );
 
 const run = () => {
-  const container = document.getElementById("container");
-  if (!container) return;
-  Array.from(container?.getElementsByClassName("js-chirp-container")).forEach(
+  Array.from(document.getElementsByClassName("chirp-container")).forEach(
     (it) => observer.observe(it, { childList: true })
   );
   console.log("SpeakTweetDeck: loaded.");
+  Array.from(document.getElementsByClassName("tweet")).forEach((it) =>
+    addSpeakButton(it)
+  );
 };
 
 setTimeout(run, 8000);
