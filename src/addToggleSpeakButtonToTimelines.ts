449b2869e86e2d3b7a5372bd33f158557e38e82f
nnnnusui <nnnnusui@gmail.com>
2021-05-15 02:46:36 +0900

    update: observe shaw timeline detail (#7)

--- src/addToggleSpeakButtonToTimelines.ts
+++ src/addToggleSpeakButtonToTimelines.ts
@@ -1,8 +1,15 @@
 import { Timeline } from "./tweet/Timeline";
 import { WhiteList } from "./WhiteList";
 
-const addToTimelineHeader = (timelineHeader: Element) => {
-  const timeline = Timeline.fromElement(timelineHeader);
+const name = "toggle-speak-button";
+const addToTimelineHeader = (source: Element) => {
+  const title = source.getElementsByClassName("column-title")[0];
+  if (!title) return;
+  if (Array.from(title.children).find((it) => it.classList.contains(name)))
+    return;
+
+  const timeline = Timeline.fromElement(source);
+  console.log(timeline);
   const button = document.createElement("a");
   button.href = "#";
   button.title = "toggle speak";
@@ -18,13 +25,26 @@ const addToTimelineHeader = (timelineHeader: Element) => {
     }
   };
   const container = document.createElement("div");
+  container.classList.add(name);
   container.appendChild(button);
-  timelineHeader.firstChild?.after(container);
+  title?.firstChild?.after(container);
 };
 
+const observer = new MutationObserver((mutations) =>
+  mutations.forEach((mutation) => {
+    const timeline = mutation.target as Element;
+    if (timeline.classList.contains("js-column-state-detail-view")) return;
+    addToTimelineHeader(timeline);
+  })
+);
+
 export const addToggleSpeakButtonToTimelines = (): void => {
-  const timelineHeaders = Array.from(
-    document.getElementsByClassName("column-title")
-  );
-  timelineHeaders.forEach(addToTimelineHeader);
+  const timelines = Array.from(document.getElementsByClassName("column"));
+  timelines.forEach((it) => {
+    addToTimelineHeader(it);
+    observer.observe(it, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+  });
 };
